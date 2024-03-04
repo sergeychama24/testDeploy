a676392bba9b33b7cf243110f9c0a86307e1a1a6
sergeychama24 <sergeychama24@gmail.com>
2024-03-04 09:00:49 +0300

    initial

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello There!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
